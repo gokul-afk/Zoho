ccd1a1e1e5172574583c1303cbd71d2efabb0949
GOKUL BIJU <gokulbiju2022@it.ajce.in>
2022-08-22 22:12:06 +0530

    hh

--- Carrot.py
+++ Carrot.py
@@ -88,3 +88,37 @@ if (dist != -1):
  
 else:
     print("Shortest Path doesn't exist")
+
+'''
+Q5. Where's the carrot?
+'Kittu' (name) rabbit needs help in finding shortest path to the carrot.
+Given a MxN matrix where each element can either be 0 or 1. We need to find the
+shortest path between Kittu’s pos(0,0) to Carrot’s pos(M,N). The path can only be
+created out of a cell if its value is 1.
+Input:
+ [[1, 0, 1, 1, 1, 1, 0, 1, 1, 1 ],
+[1, 0, 1, 0, 1, 0, 1, 0, 1, 1 ],
+[1, 1, 0, 0, 1, 1, 0, 1, 0, 1 ],
+[0, 0, 0, 0, 1, 0, 1, 0, 0, 1 ],
+[1, 1, 1, 0, 1, 1, 1, 0, 1, 0 ],
+[1, 0, 1, 1, 1, 1, 0, 0, 0, 1 ],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 1 ],
+[1, 0, 1, 1, 1, 1, 0, 1, 1, 1 ],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ],
+[1, 1, 0, 0, 0, 0, 1, 0, 0, 1 ]]
+Output:
+Shortest Path is 18
+Input :
+ [[1, 0, 0, 1, 1, 1, 1, 0, 0 ],
+[1, 0, 1, 0, 0, 0, 0, 1, 0 ],
+[1, 0, 1, 0, 0, 0, 0, 1, 0 ],
+[1, 0, 1, 0, 0, 1, 0, 0, 1 ],
+[1, 0, 1, 0, 1, 0, 1, 0, 1 ],
+[1, 0, 0, 1, 0, 0, 1, 0, 1 ],
+[1, 0, 0, 0, 0, 1, 0, 0, 1 ],
+[0, 1, 1, 1, 1, 0, 0, 1, 0 ],
+[0, 0, 0, 0, 0, 0, 0, 0, 1 ]]
+Output :
+Shortest Path is 33
+
+'''
--- SortWithoutMoving.py
+++ SortWithoutMoving.py
@@ -32,3 +32,32 @@ for i in row:
     else:
         sorted.append(-1)
 print(sorted)
+
+'''
+Q3. Sort Without Moving
+    Some people are standing in a row in a park. There are trees between them whichcannot be moved.
+     Your task is to rearrange the people by their heights in a nondescending order without moving the trees.
+
+constraints:
+    ● Given row r, 5 ≤ r.length ≤ 15,
+    ● maximum height of the people could be 200.
+    ● Height of the tree is denoted as -1
+    ● [output] Sorted row with all the trees untouched.
+
+Example
+For a = [-1, 150, 190, 170, -1, -1, 160, 180], the output should be
+sortByHeight(a) = [-1, 150, 160, 170, -1, -1, 180, 190]
+
+Test Case 1:
+    input: [-1, 3, 4, -1, 1]
+    output: [-1, 1, 3, -1, 4]
+
+Test Case 2:
+    input: [-1, -1, -1, -1, -1]
+    output: [-1, -1, -1, -1, -1]
+
+Test Case 3:
+    input: [10, 9, 8, -1, 4, 3, -1, 7, 6, -1, -1, 2, 1, -1, 0]
+    output: [0, 1, 2, -1, 3, 4, -1, 6, 7, -1, -1, 8, 9, -1, 10]
+
+'''
