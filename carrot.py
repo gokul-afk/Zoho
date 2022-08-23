86cc15acbc0d207c4017cb3cc9516df568d930e1
GOKUL BIJU <gokulbiju2022@it.ajce.in>
2022-08-23 23:27:41 +0530

    Update carrot.py

--- carrot.py
+++ carrot.py
@@ -1,13 +1,13 @@
-n = 10
 
-def isValid(n, maze, x, y, res):
-    if x >= 0 and y >= 0 and x < n and y < n and maze[x][y] == 1 and res[x][y] == 0:
+def isValid(maze, x, y, res):
+    
+    if x >= 0 and y >= 0 and x < m and y < n and maze[x][y] == 1 and res[x][y] == 0:
         return True
     return False
  
-def RatMaze(n, maze, move_x, move_y, x, y, res):
+def RatMaze(maze, move_x, move_y, x, y, res):
     global count
-    if x == n-1 and y == n-1:
+    if x == m-1 and y == n-1:
         count += 1
         return True
     for i in range(8):
@@ -17,25 +17,26 @@ def RatMaze(n, maze, move_x, move_y, x, y, res):
 
         y_new = y + move_y[i]
 
-        if isValid(n, maze, x_new, y_new, res):
+        if isValid( maze, x_new, y_new, res):
  
 
             res[x_new][y_new] = 1
             count += 1
-            if RatMaze(n, maze, move_x, move_y, x_new, y_new, res):
+            if RatMaze(maze, move_x, move_y, x_new, y_new, res):
                 return True
             res[x_new][y_new] = 0
             count -= 1
     return False
  
 def solveMaze(maze):
-    res = [[0 for i in range(n)] for i in range(n)]
+
+    res = [[0 for i in range(m)] for j in range(n)]
     res[0][0] = 1
     move_x = [1,0,1,-1,1,0,-1,-1]
     move_y = [1,1,0,1,-1,-1,0,-1]  
  
-    if RatMaze(n, maze, move_x, move_y, 0, 0, res):
-        for i in range(n):
+    if RatMaze(maze, move_x, move_y, 0, 0, res):
+        for i in range(m):
             for j in range(n):
                 print(res[i][j], end=' ')
             print()
@@ -44,33 +45,37 @@ def solveMaze(maze):
  
  
 count=0
-maze = [[1, 0, 1, 1, 1, 1, 0, 1, 1, 1 ],
-[1, 0, 1, 0, 1, 0, 1, 0, 1, 1 ],
-[1, 1, 0, 0, 1, 1, 0, 1, 0, 1 ],
-[0, 0, 0, 0, 1, 0, 1, 0, 0, 1 ],
-[1, 1, 1, 0, 1, 1, 1, 0, 1, 0 ],
-[1, 0, 1, 1, 1, 1, 0, 0, 0, 1 ],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 1 ],
-[1, 0, 1, 1, 1, 1, 0, 1, 1, 1 ],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ],
-[1, 1, 0, 0, 0, 0, 1, 0, 0, 1 ]]
- 
+maze = [
+    [1, 0, 0, 1, 1, 1, 1, 0, 0 ],
+[1, 0, 1, 0, 0, 0, 0, 1, 0 ],
+[1, 0, 1, 0, 0, 0, 0, 1, 0 ],
+[1, 0, 1, 0, 0, 1, 0, 0, 1 ],
+[1, 0, 1, 0, 1, 0, 1, 0, 1 ],
+[1, 0, 0, 1, 0, 0, 1, 0, 1 ],
+[1, 0, 0, 0, 0, 1, 0, 0, 1 ],
+[0, 1, 1, 1, 1, 0, 0, 1, 0 ],
+[0, 0, 0, 0, 0, 0, 0, 0, 1 ]
+]
+
+m= len(maze)
+n= len(maze[0]) 
 solveMaze(maze)
+
 print(count)
 
 
 '''
 OUTPUT
-1 0 1 1 1 1 0 0 0 0 
-1 0 1 0 0 0 1 0 1 1
-0 1 0 0 0 0 0 1 0 1
-0 0 0 0 0 0 0 0 0 1
-0 0 0 0 0 0 0 0 1 0
-0 0 0 0 0 0 0 0 0 1
-0 0 0 0 0 0 0 0 0 1
+1 0 0 1 0 0 0 0 0 0 
+1 0 1 0 1 0 0 0 1 0
+0 1 0 0 0 1 0 1 0 1
+0 0 0 0 0 0 1 0 0 1
+0 0 0 0 0 0 0 0 1 0 
 0 0 0 0 0 0 0 0 0 1
 0 0 0 0 0 0 0 0 0 1
-0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1
+0 0 0 0 0 0 0 0 0 1 
+0 0 0 0 0 0 0 0 0 1
 18
 
 
@@ -83,5 +88,6 @@ OUTPUT
 [1, 0, 0, 0, 0, 1, 0, 0, 1 ],
 [0, 1, 1, 1, 1, 0, 0, 1, 0 ],
 [0, 0, 0, 0, 0, 0, 0, 0, 1 ]]
+33
 
 '''
